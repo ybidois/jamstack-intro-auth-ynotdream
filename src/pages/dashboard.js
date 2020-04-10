d5d67b0dbaeacfc8d0f48426ac045376253a7588
Yannick Bidois <YnotDream>
2020-04-10 11:19:07 +0200

    Add Netlify Identity Widget

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,18 +1,20 @@
 import React, { useEffect } from "react";
 import { Router } from "@reach/router";
+import IdentityModal from "react-netlify-identity-widget";
 import Layout from "../components/layout";
 import Profile from "../components/profile";
 import RouteBase from "../components/route-base";
 import RouteSecret from "../components/route-secret";
 import RouteLogin from "../components/route-login";
 import { navigate } from "gatsby";
+import "react-netlify-identity-widget/styles.css";
 
 const Dashboard = ({ location }) => {
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
       navigate("/dashboard/login", { replace: true });
     }
-  }, []);
+  }, [location.pathname]);
   return (
     <Layout>
       <Profile />
@@ -21,6 +23,7 @@ const Dashboard = ({ location }) => {
         <RouteSecret path="/dashboard/secret" />
         <RouteLogin path="/dashboard/login" />
       </Router>
+      <IdentityModal showDialog={true} />
     </Layout>
   );
 };
